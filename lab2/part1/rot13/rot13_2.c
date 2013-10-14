ab46300ab8eb53e815408d640c33638926b0eba0
Jonathan Lim <jflim91@gmail.com>
2013-10-14 01:17:20 -0400

    need to revert Makefile when submitting if correct

--- lab2/part1/rot13/rot13_2.c
+++ lab2/part1/rot13/rot13_2.c
@@ -3,8 +3,8 @@
  *
  * Authors: Bujar Tagani <btagani@andrew.cmu.edu>
  *          Jonathan Lim <jlim2@andrew.cmu.edu>
- *	    	Luo Wu <luow@andrew.cmu.edu>
- * Date:    SUN Oct 13 21:32:12 EDT 2013
+ *	    Luo Wu <luow@andrew.cmu.edu>
+ * Date:    Mon Oct 14 04:44:21 UTC 2013
  */
 
 #include <unistd.h>
@@ -13,47 +13,48 @@
 #define BUFLEN 128
 void rot13(char arr[], int len)
 {
-	int i = 0;
-	for (; i<=len; i++){
-		if(arr[i] >= 65 && arr[i] <= 77)
-		arr[i] = arr[i]+13;
+    int i = 0;
+    for (; i<=len; i++){
+        if(arr[i] >= 65 && arr[i] <= 77)
+	    arr[i] = arr[i]+13;
 		
-		else if(arr[i] >= 97 && arr[i] <= 109)
-		arr[i] = arr[i]+13;
+	else if(arr[i] >= 97 && arr[i] <= 109)
+	    arr[i] = arr[i]+13;
 		
-		else if (arr[i] >= 110 && arr[i] <= 122)
-		arr[i] = arr[i]-13;
+	else if (arr[i] >= 110 && arr[i] <= 122)
+ 	    arr[i] = arr[i]-13;
 		
-		else if(arr[i] >= 78 && arr[i] <= 90)
-		arr[i] = arr[i]-13;
-	}
+	else if(arr[i] >= 78 && arr[i] <= 90)
+	    arr[i] = arr[i]-13;
+    }
 }
 
-int main(void){
-	char buf[BUFLEN];
-	int nread = 0;
+int main(int argc, char **argv){
+    
+    char buf[BUFLEN];
+    int nread = 0;
 	
-	/* main loop */
+    /* main loop */
     while( (nread = read(STDIN_FILENO, buf, BUFLEN)) > 0){
 	
-		/* encrypt the input */
-		rot13(buf, nread);	
+	/* encrypt the input */
+	rot13(buf, nread);	
 	
-		int nwrite;
-		int nleft = nread;
+	int nwrite = 0;
+	int nleft = nread;
 		
-		/* write the encrypted input to STDOUT */
-		while(nleft > 0){
-			if((nwrite = write(STDOUT_FILENO, buf, nleft)) < 0)
-				exit(1);
-			nleft -= nwrite;
-		}
+	/* write the encrypted input to STDOUT */
+	while(nleft > 0){
+	    if((nwrite = write(STDOUT_FILENO, buf, nleft)) < 0)
+	        exit(1);
+		nleft -= nwrite;
+	    }
 	}
 
 	if(nread == 0) 
-		exit(0);
+	    exit(0);
 	else 
-		exit(1);
+	    exit(1);
 
 	return 0; /* unreachable */
 }
