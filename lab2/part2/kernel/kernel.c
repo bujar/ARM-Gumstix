d2c990c59086b678745a0b73d7bda624acb5fc29
Norman Wu <luow.cmu@gmail.com>
2013-10-24 02:11:28 -0400

    updated kernel.c with opcodes

--- lab2/part2/kernel/kernel.c
+++ lab2/part2/kernel/kernel.c
@@ -7,7 +7,7 @@
  */
 
 #define SWI_VECTOR 0x08
-#define UBOOT_SWI 0x28
+#define UBOOT_SWI 0x28		
 //#define LDR_OPCODE (opcode for the instruction LDR pc, [pc, #-4]
 
 int main(int argc, char *argv[]) {
@@ -26,7 +26,7 @@ int install_custom_handler(Custom_S_Handler)
 * S_Handler.S function
 */
 
-*UBOOT_SWI = LDR_OPCODE
+*UBOOT_SWI = 0xe51ff004			//optcode for ldr pc, [pc, #-4]
 *(UBOOT_SWI + 1) = Custom_S_Handler
 
 }
