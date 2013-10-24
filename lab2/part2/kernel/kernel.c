88901fa6d3aeda205d3a6d85a700324c27fd5d21
Norman Wu <luow.cmu@gmail.com>
2013-10-24 02:24:26 -0400

    add a check before install our handler

--- lab2/part2/kernel/kernel.c
+++ lab2/part2/kernel/kernel.c
@@ -19,12 +19,22 @@ install_custom_handler((void *) S_Handler);	//takes address our S_HAndler.S func
 
 int install_custom_handler(Custom_S_Handler)
 {
+
+/* 
+*check whether SWI vector at 0x8 contains a instruction like ldr pc, [pc, #imm12] 
+*if contains, isntall handler
+*else, exit with status 0x0badc0de 
+*/
+
+
+
 /*
 * go to mem addr of UBoot SWI Handler
 * and modify first instruction to change pc
 * and 2nd instruction to address of custom 
 * S_Handler.S function
 */
+
 
 *UBOOT_SWI = 0xe51ff004			//optcode for ldr pc, [pc, #-4]
 *(UBOOT_SWI + 1) = Custom_S_Handler
