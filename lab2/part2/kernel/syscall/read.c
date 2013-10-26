fc530a9c1c28b7b5cea4d8a28f03141682e4258d
Bujar <tagani.bujar@gmail.com>
2013-10-26 13:43:16 -0400

    new read.c

--- lab2/part2/kernel/syscall/read.c
+++ lab2/part2/kernel/syscall/read.c
@@ -1,42 +1,48 @@
 #include <exports.h>
 #include <bits/fileno.h>
 #include <bits/errno.h>
+/*
+int main()
+{
+char buf[50];
+read(0, buf, 50);
+return 0;
+}
+*/
 
 ssize_t read (int fd, void *buf, size_t count)
 {
-	int c, bytes_read = 0;
+	int c, bytes_read;
 	char *cbuf = (char *)buf;
-
+	
 	if (count > 0x4000000)
 	return -EFAULT;
 
-	if ( fd != STDIN_FILENO)
+	if (fd != STDIN_FILENO)
 	return -EBADF;
-
-	while(((c = getc()) != 4) && (bytes_read < count)) {	//EOT = 4
-		if (c == 127 && bytes_read>0) {		//ascii code for backspace
-			bytes_read--;
-			cbuf[bytes_read] = '\0'; 	//remove last char
-			puts("\b \b");		//indicate backspace was pressed
-	
-		}
-
-		else if ( (c == '\n') || (c == '\r')) {
-			putc('\n');
-			cbuf[bytes_read] = c;
-			bytes_read++;
-			return bytes_read;
-		}
 		
-		else {
+	for(bytes_read = 0; ((c=getc()) != 4) && (bytes_read<count); bytes_read++)
+	{
+		switch (c)
+		{
+		case '\n':
+		case '\r':
 			putc(c);
 			cbuf[bytes_read] = c;
 			bytes_read++;
+			printf("%d",bytes_read);
+			return bytes_read;
+			break;
+		case 127:
+			bytes_read-=2;
+			cbuf[bytes_read] = '\0';
+			puts("\b \b");
+		default:
+			putc(c);
+			cbuf[bytes_read] = c;
 		}
 	}
-
-	cbuf[bytes_read] = 0;	//add null character
-
+//	printf("%d",bytes_read);
+	cbuf[bytes_read+1] = 0;
 	return bytes_read;
 }
-
