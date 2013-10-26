ed91eebc2254c5eb26c0eb2b0010b7beb220f38f
Jonathan Lim <jflim91@gmail.com>
2013-10-26 16:48:33 -0400

    write test

--- lab2/part1/writetest/writetest.c
+++ lab2/part1/writetest/writetest.c
@@ -3,14 +3,31 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define LEN 10000
 int main(){
 
-  char buf[50];
+  char buf[LEN];
   int i;
   //buf =  malloc (sizeof(char) * 10);
-  for(i = 0; i < 10; i++)
-	buf[i] = i + 'a'; 
-  write(1, buf, 10);
-  return 1;
+  for(i = 0; i < LEN; i+=2){
+	 buf[i] = 'a';
+	 buf[i+1] = 'b';
+  }
+
+  buf[LEN-1] = 'c';
+  
+  //tests that we can write 10000 a b ... a c
+  write(1, buf, LEN);  
+  
+  write(1, "now we are going to print buf2\n", 31); 
+  
+  // tests what happens when we write count 
+  char buf2[10] = {'1','2','3','4','5','6','7','8','9'};
+  write(1, buf2, 10);  
+
+  // tests what happens when we write count 20 when we have buffer 10
+  char buf3[10] = {'a','b','c','d','e','f','g','h','i'};
+  return write(1, buf3, 20);  
+   
 }
 
