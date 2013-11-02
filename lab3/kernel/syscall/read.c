6467f01542155933bce7a46f06d983ab2ed2c69a
Bujar <tagani.bujar@gmail.com>
2013-11-02 19:16:42 -0400

    coding style

--- lab3/kernel/syscall/read.c
+++ lab3/kernel/syscall/read.c
@@ -3,65 +3,71 @@
  * Authors: Bujar Tagani <btagani@andrew.cmu.edu>
  *  	    Jonathan Lim <jlim2@andrew.cmu.edu>
  *	    Norman Wu <luow@andrew.cmu.edu>
- * Date	    Oct 26, 2013
+ * Date: Sat Nov  2 19:03:43 EDT 2013
  */
 
 #include <exports.h>
 #include <bits/fileno.h>
 #include <bits/errno.h>
 
+#define BACKSPACE 127
+#define UBOOT_EOT 4
 #define SDRAM_END 0xa3ffffff
 #define SDRAM_START 0xa0000000
 
 ssize_t read (int fd, void *buf, size_t count)
 {
-	char c;
-	ssize_t b_read;
-	char *cbuf = (char *)buf;
-	unsigned int buf_begin, buf_end;
+  char c;
+  ssize_t b_read = 0;
+  char *cbuf = (char *)buf;
+  unsigned int buf_begin;
+  unsigned int buf_end;
 	
-	buf_begin = (unsigned int) buf;
-	buf_end = ((unsigned int) buf) + count;
+  buf_begin = (unsigned int) buf;
+  buf_end = ((unsigned int) buf) + count;
 
-	if (buf_begin < SDRAM_START || buf_end > SDRAM_END)
-	return -EFAULT;
+  if (buf_begin < SDRAM_START || buf_end > SDRAM_END) {
+  return -EFAULT;
+  }
 
-	if (fd != STDIN_FILENO)
-	return -EBADF;
-		
-	/* This will get user input with getc and keep looping to get characters
-	 * until user input is EOT or newline/return. c=getc() stores input into
-	 * char c.
-	 */
-	for(b_read = 0; ((c=getc()) != 4) && ((size_t) b_read<count); b_read++)
-	{	
-		switch (c)
-		{
-		   case '\n':
-		   case '\r':
-		   {
-		       putc('\n');
-			   cbuf[b_read] = c;
-			   b_read++;
-			   return b_read;
-		   }	// return/newline case
-		   case 127:
-		   {
-		       if (b_read>0)	
-		           b_read-=2;
-		       putc('\b');
-		       putc(' ');
-		       putc('\b');
-		       break;
-		   }	// backspace/delete case
+  if (fd != STDIN_FILENO) {
+  return -EBADF;
+  }		
+  
+  /* This will get user input with getc and keep looping to get characters
+   * until user input is EOT or newline/return. c=getc() stores input into
+   * char c.
+   */
+  for(; ((c=getc()) != UBOOT_EOT) && ((size_t) b_read<count); b_read++)
+  {	
+     switch (c)
+     {
+	case '\n':
+	case '\r':
+	{
+	   putc('\n');
+	   cbuf[b_read] = c;
+	   b_read++;
+	   return b_read;
+	}	// prints '\n' to stdout and returns to user program
+	
+	case BACKSPACE:
+	{
+	   if (b_read>0)	
+	   b_read-=2;
+	   putc('\b');
+	   putc(' ');
+	   putc('\b');
+	   break;
+	}	//prints '\b' char to stdout
 			
-		   default:
-		   {
-			putc(c);
-			cbuf[b_read] = c;
-		   }	//default keeps looping until EOT
-		}
-    }
+	default:
+	{
+	   putc(c);
+	   cbuf[b_read] = c;
+	}	//default keeps looping until EOT
+     }
+  }
 
-	return b_read;	//only reach this when c = EOT
+  return b_read;	//only reach this when c = EOT
 }
--- lab3/kernel/syscall/write.c
+++ lab3/kernel/syscall/write.c
@@ -1,8 +1,8 @@
-/*
+/* write.c: Linux system call for "write"
  * Author: Bujar Tagani <btagani@andrew.cmu.edu>
  *         Jonathan Lim <jlim2@andrew.cmu.edu>
  *         Norman Wu <luow@andrew.cmu.edu>
- *  Date:  Oct 25. 2013
+ * Date: Sat Nov  2 18:54:08 EDT 2013
  */
 
 /*
@@ -19,45 +19,39 @@
 
 #define MAX_WRITE_SIZE  0x4000000	//64MB; size of SDRAM
 #define FREE_BEGIN_ADDR 0xa0000000
+#define FREE_END_ADDR 0xa3FFFFFF
 
 ssize_t write(int fd, const void *buf, size_t count)
 {
-  unsigned int i, buf_begin, buf_end;
+  unsigned int i;
+  unsigned int buf_begin;
+  unsigned int buf_end;
 
-  // 1. check the file descriptor 
   if(fd != STDOUT_FILENO)
      {  
 	puts("file descriptor error!");	
 	return -EBADF;
-     }
- 
-  // 2. check the size of buf and the address of buf
-  if(count > MAX_WRITE_SIZE)	//check size of buf 
+     } // 1. check the file descriptor
+  
+  if(count > MAX_WRITE_SIZE)
      {
 	puts("count size err!");
 	return -EFAULT;	
-     }
+     } // 2. check the size of buf and the address of buf
 
   buf_begin = (int) buf;		//low bound of buf
   buf_end = ((int) buf) + count;	//high bound of buf
-
-  //check if the buffer exceeds the SDRAM, it automaticlly ignores NULL pointer
-  if((buf_begin < FREE_BEGIN_ADDR) || (buf_end > 0xa3ffffff))
+  if((buf_begin < FREE_BEGIN_ADDR) || (buf_end > FREE_END_ADDR))
      {
 	puts("buf address range error!");
 	return -EFAULT;	
-     }
-  
-
-  // 3. write characters from the buffer to stdout until the buffer is empty
+     } //check if the buffer exceeds the SDRAM
+   
   for(i = 0; i < count; i++) 		 
      {
 	putc(((char *)buf)[i]); 			
-     }
-				
+     }  // 3. write characters from buffer to stdout until buffer is empty
+ 			
   // 4. return with the number of characters to stdout
   return count;
 }
-
-	
-
