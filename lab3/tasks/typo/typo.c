21cdba62de8de3bff63c23e77bccbfd3bf0c8be2
Jonathan Lim <jflim91@gmail.com>
2013-11-06 23:26:13 -0500

    update conversion

--- lab3/tasks/typo/typo.c
+++ lab3/tasks/typo/typo.c
@@ -13,34 +13,35 @@
 #include <stdlib.h>
 
 #define BUFLEN 128
-
+#define MILLI_TO_SEC 1000
 int main (int argc, char **argv) {
 
-   unsigned long start_time_ms, end_time_ms;
-   char input_buf[BUFLEN];
-   int nleft = 0;
-   int nwrite = 0;
-   int nread = 0;
-   unsigned long time_elapsed = 0, time_elapsed_s, time_elapsed_ms;
-   start_time_ms = time();
+	while(1){
+		unsigned long start_time_ms, end_time_ms;
+		char input_buf[BUFLEN];
+		int nleft = 0;
+		int nwrite = 0;
+		int nread = 0;
+   		unsigned long time_elapsed = 0; 
+		unsigned long time_elapsed_s, time_elapsed_ds;
+   		start_time_ms = time();
    
-   putchar('>');
-   nread = read(STDIN_FILENO, input_buf, BUFLEN);
+   		putchar('>'); //print prompt
+   		nread = read(STDIN_FILENO, input_buf, BUFLEN);
 	
-   nleft = nread;
-   while (nleft>0){
-      if ((nwrite = write(STDOUT_FILENO, input_buf, nleft)) < 0)
-         exit(1);
-         nleft -= nwrite;
+   		nleft = nread;
+   		while (nleft>0){
+      		if ((nwrite = write(STDOUT_FILENO, input_buf, nleft)) < 0)
+         		exit(1);
+         	nleft -= nwrite;
+   		}
+
+   		end_time_ms = time();
+     	time_elapsed = end_time_ms - start_time_ms;
+   		time_elapsed_s  = time_elapsed / MILLI_TO_SEC;
+   		time_elapsed_ds = (time_elapsed % MILLI_TO_SEC) / 100;
+
+   		printf("%lu.%lus", time_elapsed_s, time_elapsed_ds);
    }
-
-	
-   end_time_ms = time();
-   time_elapsed = end_time_ms - start_time_ms;
-   time_elapsed_s = time_elapsed/1000;
-   time_elapsed_ms = (time_elapsed/1000)%10;
-
-   printf("Time Elapsed: %lu.%lu\ns", time_elapsed_s, time_elapsed_ms);
-
    return 0;
 }
