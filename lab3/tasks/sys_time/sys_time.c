6dbb069ff64420923d0ba7c768d5faed92120052
Bujar <tagani.bujar@gmail.com>
2013-11-10 21:23:15 -0500

    formatting sys_time

--- lab3/tasks/sys_time/sys_time.c
+++ lab3/tasks/sys_time/sys_time.c
@@ -13,15 +13,16 @@
 #include <stdlib.h>
 
 int main(int argc, char** argv){
-	unsigned long temp_ms, temp_s; 
-	if(argc != 2){
-		printf("sys_time [milliseconds]\n");
-		exit(1);
-	}
-	sleep(atoi(argv[1]));
-    temp_ms = time();
-	temp_s =  temp_ms / 1000;
-	temp_ms %= 1000;
-    printf("Our System has been running for %ld.%ld S \n ", temp_s,temp_ms);
-	return 0;
+   unsigned long temp_ms, temp_s; 
+   if(argc != 2){
+	printf("sys_time [milliseconds]\n");
+	exit(1);
+   }
+   
+   sleep(atoi(argv[1]));
+   temp_ms = time();
+   temp_s =  temp_ms / 1000;
+   temp_ms %= 1000;
+   printf("System has been running for %ld.%ld S \n ", temp_s,temp_ms);
+   return 0;
 }
