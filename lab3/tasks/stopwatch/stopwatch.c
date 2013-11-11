5310065b863bbbb6f154f3b0f1a80ebf507b7168
Bujar <tagani.bujar@gmail.com>
2013-11-10 19:23:04 -0500

    updated stopwatch

--- lab3/tasks/stopwatch/stopwatch.c
+++ lab3/tasks/stopwatch/stopwatch.c
@@ -1,6 +1,6 @@
 /** @file stopwatch.c
  *
- * @This program is used to calculate the time for running.
+ * @This is a simple stopwatch program that records times
  *
  * @author: Bujar Tagani <btagani@andrew.cmu.edu>
  *          Jonathan Lim <jlim2@andrew.cmu.edu>
@@ -10,59 +10,129 @@
 
 #include <unistd.h>
 #include <stdio.h>
-#include <stdlib.h>
 
+#define BUFLEN 128
 #define MS_PER_S 1000
 
-void usage(){
-	printf("please press s to start to count time\n");
-	printf("once it has started, please press r to record a time\n");
-	printf("and press q to quit the stopwatch\n");
-}
+/* Globals */
+int record_num = 0;
+int keep_running = 1;
+unsigned long record_buf_s[BUFLEN];
+unsigned long record_buf_ms[BUFLEN];
+
+/* Functions */
+void print_results();
+void stopwatch();
+void menu();
 
 int main(int argc, char** argv){
-	char a;
-	int recordnum = 0, started = 0;
-	unsigned long start_time = 0, end_time = 0, tmp_s = 0, tmp_ms = 0;
+   char r;
+   stopwatch();
 
+   /*Menu once first stopwatch is finished */
+   while(1) {
+	printf("\n\n\n\nWhat would you like to do?\n");
+	printf("R - Restart\n");
+	printf("P - Print Results\n");
+	printf("Q - Quit Application\n");
+	read(0, (void *)(&r), 1);
 
-	while(1){
-		read(0, (void *)(&a), 1);
-		if((started == 0) && (a == 'r'))
-		{
-			printf(" is the input, please start time watch first\n");
-			continue;	
-		}
+	switch(r) {
+	   case 'r':
+	   case 'R':
+	   	keep_running = 1;	//sets keep_running so we can run stopwatch again
+	   	stopwatch();
+	   	break;
+	   case 'p':
+	   case 'P':
+		print_results();
+		break;
+	   case 'q':
+	   case 'Q':
+		return 0;
+	   default:
+		return 0;
+	}
+   }
+}
+
+/* takes user input and stores recorded times into buffer */
+void stopwatch()
+{
+  char a;
+  int started = 0;
+  unsigned long start_time = 0;
+  unsigned long end_time = 0;
+  unsigned long tmp_s = 0;	//used to store seconds
+  unsigned long tmp_ms = 0;	//used to store milliseconds
 	
-		switch(a){
-			case 's':
-				if(started == 1)
-				{
-					printf(" is the input, stopwatch already started\n");
-					break;	
-				}
-				started = 1;
-				start_time = time();
-				printf(" is the input, stopwatch started...\n");
-				break;
+  record_num = 0;		//resets record_num if running function more than once
 
-			case 'q':
-				printf(" is the input, gonna quit...\n");
-				return 1;
+  menu();
 
-			case 'r':
-				end_time = time();
-				tmp_ms = end_time - start_time;
-				tmp_s = tmp_ms / MS_PER_S;
-				tmp_ms %= MS_PER_S;
-				recordnum++;
-				printf(" is the input\t");
-				printf("Record %d: %ld.%lds \n", recordnum, tmp_s, tmp_ms);
-				break;
-			default:
-				printf("unsupported command\n");
-				usage();
-		}
-	}	
-	return 0;
+  while(keep_running == 1){
+     read(0, (void *)(&a), 1);
+     if((started == 0) && (a == 'r'))
+     {
+	printf("You must start first\n");
+	continue;	
+     }
+	
+     switch(a){
+	case 's':
+	case 'S':
+	   if(started == 1)
+	   {
+		printf(" Stopwatch already started\n");
+		break;	
+	   }
+	   started = 1;
+	   start_time = time();
+	   printf(" Stopwatch Started\n");
+	   break;
+
+	case 'q':
+	case 'Q':
+	   printf("\n\n\nFinished Recording\n");
+	   keep_running = 0;
+	   break;
+
+	case 'r':
+	case 'R':
+	   end_time = time();
+	   tmp_ms = end_time - start_time;
+	   tmp_s = tmp_ms / MS_PER_S;
+	   tmp_ms %= MS_PER_S;
+	   record_buf_s[record_num] = tmp_s;	//seconds buffer
+	   record_buf_ms[record_num] = tmp_ms;	//milliseconds buffer
+	   record_num++;
+	   printf("ecord %d: %ld.%lds \n", record_num, tmp_s, tmp_ms);
+	   break;
+
+  	default:
+	   printf("Unsupported Commandi\n\n");
+	   menu();
+	}
+   }	
+	
+}
+
+/* prints results of last running stopwatch() */
+void print_results()
+{
+  int index = 0;
+  for(; index < record_num; index++) {
+     printf("\n\nRecorded Time %d:  ", index+1);
+     printf("%lu.", record_buf_s[index]);
+     printf("%lus", record_buf_ms[index]);
+   }
+}
+
+/* main options menu */
+void menu(){
+  printf("\nWelcome to the Stopwatch. Input an option and press Enter\n");
+  printf("\n******Options******\n");
+  printf("S - Start\n");
+  printf("R - Record Time\n");
+  printf("Q - Quit\n");
 }
