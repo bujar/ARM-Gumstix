cf3e64757a9c8b53d5294caab4a9d35db36b049f
Norman Wu <luow.cmu@gmail.com>
2013-11-24 15:32:08 -0500

    add NULL

--- lab4/kernel/lock/mutex.c
+++ lab4/kernel/lock/mutex.c
@@ -17,9 +17,12 @@
 #include <bits/errno.h>
 #include <arm/psr.h>
 #include <arm/exception.h>
+
 #ifdef DEBUG_MUTEX
 #include <exports.h> // temp
 #endif
+
+#define NULL 0
 
 mutex_t gtMutex[OS_NUM_MUTEX];
 
@@ -40,18 +43,25 @@ int mutex_create(void)
 	uint32_t i = 0;
 	for(i = 0; i < OS_NUM_MUTEX; i++)
 	{
-		if(gtMutex[i].bAvailable = TRUE)
+		if(gtMutex[i].bAvailable == TRUE)
 		{	
 			gtMutex[i].bAvailable  = FALSE;
 			return i;
 		}
 	}	
-	return ENOMEN; 
+	return -ENOMEM; 
 }
 
 int mutex_lock(int mutex  __attribute__((unused)))
-{
-
+{	
+	/* check if the mutex number within the mutex area, and check if the mutex was created */
+	if((mutex > OS_NUM_MUTEX) || (mutex < 0) || gtMutex[mutex].bAvailable)
+	{	
+		return -EINVAL;	
+	}
+	
+	/*   */
+	
 	return 1; // fix this to return the correct value
 }
 
