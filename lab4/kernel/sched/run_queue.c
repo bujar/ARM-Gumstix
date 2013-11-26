1bbf13873614441f967b127aa5c202a21a367858
Jonathan Lim <jflim91@gmail.com>
2013-11-26 00:59:14 -0500

    cleaned some code in run_queue

--- lab4/kernel/sched/run_queue.c
+++ lab4/kernel/sched/run_queue.c
@@ -15,24 +15,24 @@
 
 #define NULL 0
 
-static tcb_t* run_list[OS_MAX_TASKS]  __attribute__((unused));
+static tcb_t* run_list[OS_MAX_TASKS];
 
 /* A high bit in this bitmap means that the task whose priority is
  * equal to the bit number of the high bit is runnable.
  */
-static uint8_t run_bits[OS_MAX_TASKS/8] __attribute__((unused));
+static uint8_t run_bits[OS_MAX_TASKS/8];
 
 /* This is a trie structure.  Tasks are grouped in groups of 8.  If any task
  * in a particular group is runnable, the corresponding group flag is set.
  * Since we can only have 64 possible tasks, a single byte can represent the
  * run bits of all 8 groups.
  */
-static uint8_t group_run_bits __attribute__((unused));
+static uint8_t group_run_bits;
 
 /* This unmap table finds the bit position of the lowest bit in a given byte
  * Useful for doing reverse lookup.
  */
-static uint8_t prio_unmap_table[]  __attribute__((unused)) =
+static uint8_t prio_unmap_table[] =
 {
 
 0, 0, 1, 0, 2, 0, 1, 0, 3, 0, 1, 0, 2, 0, 1, 0,
