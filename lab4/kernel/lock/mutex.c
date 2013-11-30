c737bd783720f5a9774a9c231016d0959611bbed
Norman Wu <luow.cmu@gmail.com>
2013-11-30 15:59:46 -0500

    fix some bugs on mutex

--- lab4/kernel/lock/mutex.c
+++ lab4/kernel/lock/mutex.c
@@ -39,6 +39,14 @@ void mutex_init()
 	}	
 }
 
+/*
+ *This function returns a unique identifier to a mutex object that the 
+ *kernel has created. If the kernel is out of identifiers, it returns an error.
+ *All valid ids are positive integers. The returned ids start at zero and 
+ *monotonically increase by one for every successful subsequent call. This 
+ *function should return successfully for at least 32 invocation.
+*/
+
 int mutex_create(void)
 {
 	uint32_t i = 0;
@@ -47,7 +55,7 @@ int mutex_create(void)
 		if(gtMutex[i].bAvailable == TRUE)
 		{	
 			gtMutex[i].bAvailable  = FALSE;
-			return i + 1;
+			return i;//should be i + 1 or i?
 		}
 	}	
 	return -ENOMEM; 
@@ -70,12 +78,14 @@ void mutex_queue_add(int mutex, tcb_t *tcb)
 		{
 			p1 = p1->sleep_queue;
 		}
-		p1 = tcb;
+		p1->sleep_queue = tcb;
 		tcb->sleep_queue = NULL;
-	}	
+	}
+	/* remove the tcb from runqueue */
+	runqueue_remove(tcb->cur_prio);	
 }
 
-/* remove a task in mutex sleeping queue when a mutex is released */
+/* remove the first task in mutex sleeping queue when a mutex is released */
 void mutex_queue_remove(int mutex)
 {
 	mutex_t *mtx = &gtMutex[mutex];
@@ -87,11 +97,14 @@ void mutex_queue_remove(int mutex)
 	{
 		if(p1->sleep_queue == NULL)
 		{	
+			/* make the task runable */
 			runqueue_add(p1, p1->cur_prio);
 			mtx->pSleep_queue = NULL;
 		}else{
 			p2 = p1;
 			p1 = p1->sleep_queue;
+			mtx->pSleep_queue = p1;
+			/* make the task runable */
 			runqueue_add(p2, p2->cur_prio);	
 		}
 	}
@@ -101,8 +114,9 @@ int mutex_lock(int mutex  __attribute__((unused)))
 {	
 	tcb_t *cur_tcb;
 	 
-	/* check if the mutex number within the mutex area, and check if the mutex was created */
-	if((mutex > OS_NUM_MUTEX) || (mutex < 0) || gtMutex[mutex].bAvailable)
+	/* check if the mutex number within the mutex area, and check if the mutex 
+	was created */
+	if((mutex >= OS_NUM_MUTEX) || (mutex < 0) || gtMutex[mutex].bAvailable)
 	{	
 		return -EINVAL;	
 	}
@@ -114,7 +128,7 @@ int mutex_lock(int mutex  __attribute__((unused)))
 		return -EDEADLOCK;
 	}
 
-	/*   */
+	/* try to get the mutex */
 	if(gtMutex[mutex].bLock == FALSE)
 	{	
 		gtMutex[mutex].bLock = TRUE;
@@ -132,16 +146,17 @@ int mutex_unlock(int mutex  __attribute__((unused)))
 {
 	tcb_t *cur_tcb;
 	 
-	/* check if the mutex number within the mutex area, and check if the mutex was created */
-	if((mutex > OS_NUM_MUTEX) || (mutex < 0) || gtMutex[mutex].bAvailable)
+	/* check if the mutex number within the mutex area, and check if the mutex 
+	was created */
+	if((mutex >= OS_NUM_MUTEX) || (mutex < 0) || gtMutex[mutex].bAvailable)
 	{	
 		return -EINVAL;	
 	}
 	
 	cur_tcb = get_cur_tcb();
 
-	/* Check if the current task is already holding the lock */
-	if(cur_tcb == gtMutex[mutex].pHolding_Tcb)
+	/* Check if the current task is holding the lock */
+	if(cur_tcb != gtMutex[mutex].pHolding_Tcb)
 	{
 		return -EPERM;
 	}
